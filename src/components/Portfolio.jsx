ac94b0ca9ec3db707d4b3ae4d811ff89345c268b
Mikayla <user@users-MacBook-Pro.local>
2023-09-11 16:34:23 -0400

    uploaded portfolio images for cards

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,7 +12,8 @@ export const Portfolio = () => {
 
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
             <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center max-auto content-div'>
-                
+            
+            {/* Hover Effect */}
                 <div>
                     <span>
 
