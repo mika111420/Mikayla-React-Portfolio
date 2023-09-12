457ba5a04edda0f3ecc4ba3b3002717f2aab7607
Mikayla <user@users-MacBook-Pro.local>
2023-09-12 15:02:44 -0400

    created footer and configured to meet requirements

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Portfolio />
       <Contact />
+      <Footer />
     </div>
   );
 }
